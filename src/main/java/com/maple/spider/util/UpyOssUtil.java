768a531296eb6244cb6ada0734160377875316a8
zhangfuzeng <zhangfuzeng@cangoonline.com>
2022-08-12 16:31:17 +0800

    添加图片压缩

--- src/main/java/com/maple/spider/util/UpyOssUtil.java
+++ src/main/java/com/maple/spider/util/UpyOssUtil.java
@@ -3,12 +3,15 @@ package com.maple.spider.util;
 import com.upyun.RestManager;
 import com.upyun.UpException;
 import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnails;
 import okhttp3.Response;
-import org.apache.commons.io.IOUtils;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Calendar;
 
 
@@ -23,7 +26,7 @@ public class UpyOssUtil {
     private static final String BUCKET_NAME = "";
     private static final String OPERATOR_NAME = "";
     private static final String OPERATOR_PWD = "";
-    private static final String URL = "";
+    private static final String SHOW_URL = "";
 
 
     /**
@@ -43,12 +46,25 @@ public class UpyOssUtil {
         RestManager restManager = new RestManager(BUCKET_NAME, OPERATOR_NAME, OPERATOR_PWD);
 
         URI u = URI.create(url);
-        try (InputStream inputStream = u.toURL().openStream()) {
+        try (InputStream inputStream = u.toURL().openStream();
+             ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
 
-            byte[] bytes = IOUtils.toByteArray(inputStream);
-            Response response = restManager.writeFile(fileName, bytes, null);
+            URL urlFile = new URL(url);
+            URLConnection conn = urlFile.openConnection();
+            int size = conn.getContentLength();
+
+            if (size > 1000 * 1024) {
+                Thumbnails.of(inputStream).scale(0.8f).outputFormat("jpg").outputQuality(0.5).toOutputStream(outputStream);
+            } else if (size > 500 * 1024) {
+                Thumbnails.of(inputStream).scale(0.8f).outputFormat("jpg").outputQuality(0.8).toOutputStream(outputStream);
+            } else if (size > 100 * 1024) {
+                Thumbnails.of(inputStream).scale(0.9f).outputFormat("jpg").outputQuality(0.8).toOutputStream(outputStream);
+            } else {
+                Thumbnails.of(inputStream).scale(1f).outputQuality(1).toOutputStream(outputStream);
+            }
+            Response response = restManager.writeFile(fileName, outputStream.toByteArray(), null);
             if (response.isSuccessful()) {
-                return URL + fileName;
+                return SHOW_URL + fileName;
             } else {
                 return url;
             }
@@ -64,5 +80,4 @@ public class UpyOssUtil {
         }
         return ".jpg";
     }
-
 }
